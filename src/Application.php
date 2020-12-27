22a2a336f935bb75333b6af58be0e60eaa789499
chop1k <fakeshopendesu@gmail.com>
2020-12-27 07:34:22 -0600

    added comments

--- src/Application.php
+++ src/Application.php
@@ -5,37 +5,79 @@ namespace Chizu\App;
 use Chizu\DI\Container;
 use Chizu\Event\Event;
 use Chizu\Event\Events;
-use Chizu\Module\Modules;
+use Ds\Map;
 
+/**
+ * Class Application represents main application class.
+ *
+ * @package Chizu\App
+ */
 class Application
 {
+    /**
+     * Executes when application starts.
+     */
     public const StartEvent = 'application.start';
+
+    /**
+     * Executes when application ends.
+     */
     public const EndEvent = 'application.end';
 
+    /**
+     * Contains application event dispatcher.
+     *
+     * @var Events $events
+     */
     protected Events $events;
 
+    /**
+     * Returns application event dispatcher.
+     *
+     * @return Events
+     */
     public function getEvents(): Events
     {
         return $this->events;
     }
 
+    /**
+     * Contains dependency injection container.
+     *
+     * @var Container $container
+     */
     protected Container $container;
 
+    /**
+     * Returns dependency injection container.
+     *
+     * @return Container
+     */
     public function getContainer(): Container
     {
         return $this->container;
     }
 
-    protected Modules $modules;
+    /**
+     * Contains map with modules.
+     *
+     * @var Map $modules
+     */
+    protected Map $modules;
 
     /**
-     * @return Modules
+     * Returns map with modules.
+     *
+     * @return Map
      */
-    public function getModules(): Modules
+    public function getModules(): Map
     {
         return $this->modules;
     }
 
+    /**
+     * Application constructor.
+     */
     public function __construct()
     {
         $this->events = new Events();
@@ -44,19 +86,28 @@ class Application
         $this->events->set(self::EndEvent, Event::createByMethod($this, 'onEnd'));
 
         $this->container = new Container();
-        $this->modules = new Modules();
+        $this->modules = new Map();
     }
 
+    /**
+     * Executes when start event dispatched.
+     */
     protected function onStart(): void
     {
 
     }
 
+    /**
+     * Executes when end event dispatched.
+     */
     protected function onEnd(): void
     {
 
     }
 
+    /**
+     * Executes start and end events.
+     */
     public function start(): void
     {
         $this->events->get(self::StartEvent)->execute();
